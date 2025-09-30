ab24ed98acbf73fe085fa95b20426980f86619a7
Kimiberry <jamieleetaggart643@gmail.com>
2025-09-30 08:54:02 -0400

    Update stdafx.h
    Included Windows.h
    This may fix a few things

--- languageGetSimple/stdafx.h
+++ languageGetSimple/stdafx.h
@@ -7,9 +7,11 @@
 
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+#include <windows.h>
 #include <stdio.h>
 #include <tchar.h>
 
 
 
 // TODO: reference additional headers your program requires here
+
